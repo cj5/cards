f562ebce95b1018b6e9f34c820a5703804f2b713
Chris <stac1221@gmail.com>
2020-12-11 00:30:43 -0600

    Client — Blackjack store: updated payout() mutation; added updatePlay() mutation

--- client/src/stores/games/blackjack.js
+++ client/src/stores/games/blackjack.js
@@ -37,6 +37,7 @@ export default {
             value: null,
             bet: null,
             bank: state.startingBank,
+            play: null,
             aces: 0,
             bust: false,
           })
@@ -56,6 +57,40 @@ export default {
       }
 
       state.cards = shuffle(cards)
+      state.cards = [
+        'c-1', // p-0
+        'd-1', // p-1
+        'd-3', // d
+        's-10', // p-0
+        'h-10', // p-1
+        'h-4', // d
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      //   'h-4',
+      ]
     },
     deal(state) {
       for (let i = 0; i < 2; i++) {
@@ -75,8 +110,39 @@ export default {
         hit(state, state.players[state.playerTurn])
       }
     },
-    payout(state, { i, amount }) {
-      state.players[i].bank += amount
+    payout(state) {
+      let dealer = state.players[state.players.length - 1]
+
+      for (let i = 0; i < state.players.length - 1; i++) {
+        let player = state.players[i]
+        let winAmount
+
+        if (player.play === 'double') {
+          player.bank -= player.bet
+          player.bet *= 2 // double current bet
+          winAmount = Math.floor((player.bet * 2))
+        } else {
+          winAmount = player.bet * 2
+        }
+
+        if (!player.bust) {
+          if (dealer.bust) {
+            console.log(`Dealer busted so Chris_${i} BEAT the dealer`)
+            state.players[i].bank += winAmount
+          } else if (player.value > dealer.value) {
+            console.log(`Chris_${i} did not bust and BEAT the dealer, ${player.value}-${dealer.value}`)
+            state.players[i].bank += winAmount
+          } else if (player.value === dealer.value) {
+            console.log(`Chris_${i} did not bust and PUSHED with the dealer`)
+            let amount = player.bet
+            state.players[i].bank += amount
+          } else if (player.value < dealer.value && !dealer.bust) {
+            console.log(`Chris_${i} did not bust but LOST to the dealer, ${dealer.value}-${player.value}`)
+          }
+        } else {
+          console.log(`Chris_${i} busted so LOST to the dealer`)
+        }
+      }
     },
     returnHands(state) {
       state.players.map(x => {
@@ -85,6 +151,7 @@ export default {
         x.bet = null
         x.aces = null
         x.bust = false
+        x.play = null
       })
     },
     bankHandout(state, i) {
@@ -99,6 +166,9 @@ export default {
     },
     setRoundState(state, str) {
       state.roundState = str
+    },
+    updatePlay(state, str) {
+      state.players[state.playerTurn].play = str
     },
   },
   // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
